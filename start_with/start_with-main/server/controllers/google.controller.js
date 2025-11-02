099b4a90770cb9833fb74fc936ad5cd8252affe3
Sahil Vaishnav🍊 <vaishnavsahil98@gmail.com>
2025-11-03 04:19:13 +0530

    urls update

--- start_with/start_with-main/server/controllers/google.controller.js
+++ start_with/start_with-main/server/controllers/google.controller.js
@@ -88,7 +88,7 @@ export const getGoogleSheets = async (req, res) => {
         const oauth2Client = new google.auth.OAuth2(
             process.env.GOOGLE_CLIENT_ID,
             process.env.GOOGLE_CLIENT_SECRET,
-            "http://localhost:4000/api/google/callback"
+            `${process.env.BACKEND_ORIGIN}/api/google/callback`
         );
 
         oauth2Client.setCredentials({
