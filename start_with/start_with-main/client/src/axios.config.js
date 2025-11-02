636e829fd8339b46cc411798edac1e9c5e95fa7a
Sahil Vaishnav🍊 <vaishnavsahil98@gmail.com>
2025-11-02 21:16:24 +0530

    urls update

--- start_with/start_with-main/client/src/axios.config.js
+++ start_with/start_with-main/client/src/axios.config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const VITE_API_BASE_URL = 'https://ac409bf3900f.ngrok-free.app';
+const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://ac409bf3900f.ngrok-free.app';
 const API = axios.create({
     baseURL: VITE_API_BASE_URL,
     withCredentials: true,
