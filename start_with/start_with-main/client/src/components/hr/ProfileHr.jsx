ab6e14c5db3a6baa77efefca4e5f5806411d574a
Sahil Vaishnav🍊 <vaishnavsahil98@gmail.com>
2025-11-04 00:23:04 +0530

    cross button working

--- start_with/start_with-main/client/src/components/hr/ProfileHr.jsx
+++ start_with/start_with-main/client/src/components/hr/ProfileHr.jsx
@@ -1071,7 +1071,7 @@ const ProfileHr = () => {
                                                     <h2 className='text-2xl font-semibold text-gray-800'>Create Interview</h2>
                                                     <p className='text-sm text-gray-500 mt-1'>Fill in the details below to create a new interview</p>
                                                 </div>
-                                                <button aria-label='Close' className='bg-gray-100 hover:bg-gray-200 rounded-lg p-2 transition-colors'>
+                                                <button onClick={() => { setInterviewCreateWindow(false) }} aria-label='Close' className='bg-gray-100 hover:bg-gray-200 rounded-lg p-2 transition-colors'>
                                                     <svg xmlns="http://www.w3.org/2000/svg" className='h-5 w-5 text-gray-600' viewBox="0 0 20 20" fill="currentColor">
                                                         <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
                                                     </svg>
