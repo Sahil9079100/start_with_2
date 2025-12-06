b3c9c52f55efad71398d818143ad824d155d54c5
Sahil Vaishnav🍊 <vaishnavsahil98@gmail.com>
2025-12-07 04:17:31 +0530

    asa

--- start_with/start_with-main/server/server.js
+++ start_with/start_with-main/server/server.js
@@ -24,7 +24,7 @@ import { pipelineWorker, interviewPipelineQueue } from "./queues/interviewPipeli
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 
-	const app = express();
+const app = express();
 
 const server = createServer(app)
 
